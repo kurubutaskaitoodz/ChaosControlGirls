762ffff6b481da58c79138b53308e3adbb00beea
Kaitoodz <reallyjedzibszboi777@gmail.com>
2024-12-27 08:13:07 +0700

    basic cursor movement

--- Assets/Scripts/KeyboardCommanding/MainKeyboardControl.cs
+++ Assets/Scripts/KeyboardCommanding/MainKeyboardControl.cs
@@ -7,23 +7,35 @@ public class MainKeyboardControl : MonoBehaviour
     private MainGameStates GState;
     private KeyboardKeys KCKeyboard;
 
+    public GameObject cursorObject;
+    public float cursorSpeed;
+    [SerializeField] private int movingX = 0;
+    [SerializeField] private int movingY = 0;
+
+    private bool setupComplete = false;
+
     public static MainKeyboardControl instance;
+
     private void OnEnable()
     {
         instance = this;
     }
 
-    private void Awake()
+    private void Start()
     {
         GState = MainGameStates.instance;
         KCKeyboard = KeyboardKeys.instance;
+        setupComplete = true;
     }
 
     private void Update()
     {
-        if (GState.isMovementAvailable)
+        if (setupComplete)
         {
-            CursorMoments();
+            if (GState.isMovementAvailable)
+            {
+                CursorMoments();
+            }
         }
     }
 
@@ -34,6 +46,51 @@ public class MainKeyboardControl : MonoBehaviour
             Debug.Log("Selecting");
         }
 
+        #region
+        if (Input.GetKeyDown(KCKeyboard.KC_movementLeft))
+        {
+            movingX--;
+        }
+        else if (Input.GetKeyUp(KCKeyboard.KC_movementLeft))
+        {
+            movingX++;
+        }
+        if (Input.GetKeyDown(KCKeyboard.KC_movementRight))
+        {
+            movingX++;
+        }
+        else if (Input.GetKeyUp(KCKeyboard.KC_movementRight))
+        {
+            movingX--;
+        }
+        if (Input.GetKeyDown(KCKeyboard.KC_movementUp))
+        {
+            movingY++;
+        }
+        else if (Input.GetKeyUp(KCKeyboard.KC_movementUp))
+        {
+            movingY--;
+        }
+        if (Input.GetKeyDown(KCKeyboard.KC_movementDown))
+        {
+            movingY--;
+        }
+        else if (Input.GetKeyUp(KCKeyboard.KC_movementDown))
+        {
+            movingY++;
+        }
+        #endregion
+
+
+        if (movingX != 0)
+        {
+            cursorObject.transform.position += new Vector3(movingX * cursorSpeed,0) * Time.deltaTime;
+        }
+
+        if (movingY != 0)
+        {
+            cursorObject.transform.position += new Vector3(0, movingY * cursorSpeed) * Time.deltaTime;
+        }
     }
 
 }
